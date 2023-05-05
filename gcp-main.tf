798f6c6a120b7409bdb69429164b275ca0f3ea51
aurimarbezerra <118943731+aurimarbezerra@users.noreply.github.com>
2023-05-04 22:45:38 -0300

    adding database

--- gcp-main.tf
+++ gcp-main.tf
@@ -4,3 +4,13 @@ resource "google_artifact_registry_repository" "my-repo" {
   description = "Imagens Docker"
   format = "DOCKER"
 }
+
+resource "google_sql_database_instance" "main" {
+  name             = "main-instance"
+  database_version = "MYSQL_8_0"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
